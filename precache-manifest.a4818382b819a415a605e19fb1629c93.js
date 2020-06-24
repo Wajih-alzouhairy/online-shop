975c36512b40b08f64cb925a3793459a0cd9f5f3
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-06-24 11:19:46 +0200

    Updates

---
+++ precache-manifest.a4818382b819a415a605e19fb1629c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0729712987f2b8e3a096f698872998f",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "fc4f8578d3e103da077c",
+    "url": "/online-shop/static/css/main.9ac4f2e2.chunk.css"
+  },
+  {
+    "revision": "d8afc8dc9873c8d88990",
+    "url": "/online-shop/static/js/2.43d1b2c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-shop/static/js/2.43d1b2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4f8578d3e103da077c",
+    "url": "/online-shop/static/js/main.67fbf51d.chunk.js"
+  },
+  {
+    "revision": "774444d9d60350fd1a25",
+    "url": "/online-shop/static/js/runtime-main.1788f7cf.js"
+  }
+]);
